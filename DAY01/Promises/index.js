04ee75309a24ff0df8b54e8262210737a292b6a4
ankur027-dev <tiwariankit121@yahoo.com>
2026-01-29 10:21:26 +0530

    first commit

---
+++ DAY01/Promises/index.js
@@ -0,0 +1,23 @@
+const mPromise = new Promise((resolve, reject) => {
+  let age = 24;
+
+  if (age>=18) {
+    resolve("eligble for vote");
+  } else {
+    reject("not eligble for note");
+  }
+});
+
+// mPromise
+//   .then(result => console.log(result))
+//   .catch(error => console.log(error));
+const f1 = async () => {
+  try {
+    const result = await mPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+f1();
