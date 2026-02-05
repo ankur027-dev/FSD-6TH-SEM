3bb4ce77b39e0974f1c273520ec44eb7467c0f15
ankur027-dev <tiwariankit121@yahoo.com>
2026-02-05 10:39:12 +0530

    Initial commit

--- DAY01/Promises/index.js
+++ DAY01/Promises/index.js
@@ -1,23 +1,28 @@
-const mPromise = new Promise((resolve, reject) => {
-  let age = 24;
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => console.log(data));
+  .catch((error)=>console.log("Unable to fetch data"));
+  .finaly(()=>console.log(data));
+// const mPromise = new Promise((resolve, reject) => {
+//   let age = 24;
 
-  if (age>=18) {
-    resolve("eligble for vote");
-  } else {
-    reject("not eligble for note");
-  }
-});
+//   if (age>=18) {
+//     resolve("eligble for vote");
+//   } else {
+//     reject("not eligble for note");
+//   }
+// });
 
-// mPromise
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error));
-const f1 = async () => {
-  try {
-    const result = await mPromise;
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// // mPromise
+// //   .then(result => console.log(result))
+// //   .catch(error => console.log(error));
+// const f1 = async () => {
+//   try {
+//     const result = await mPromise;
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-f1();
+// run();
